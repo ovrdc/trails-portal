5286539d9dc0fa508564b335740f074f6657f8f5
unknown <mmeyer@MMEYERS-WS.ovrdc.local>
2017-04-11 16:04:22 -0400

    test

---
+++ _includes/js/new-age.js
@@ -0,0 +1,39 @@
+(function($) {
+    "use strict";
+
+    /*jQuery for page scrolling feature - requires jQuery Easing plugin*/
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 150)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    /* Highlight the top nav as scrolling occurs*/
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 100
+    });
+
+    /* Closes the Responsive Menu on Menu Item Click*/
+    $('.navbar-collapse ul li a').click(function(e) {
+      $('.navbar-toggle:visible').click();
+    });
+
+    /*Fit Text Plugin for Main Header*/
+    $("h1").fitText(
+        1.2, {
+            minFontSize: '35px',
+            maxFontSize: '65px'
+        }
+    );
+
+    /* Offset for Main Navigation*/
+    $('#mainNav').affix({
+        offset: {
+            top: 50
+        }
+    })
+
+})(jQuery);
---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+---
+layout: compress
+---
+{% include js/jquery.js %}
+{% include js/jquery.easing.min.js %}
+{% include js/jquery.fittext.js %}
+{% include js/bootstrap.min.js %}
+{% include js/new-age.js %}
+{% include js/simple-jekyll-search.js %}
