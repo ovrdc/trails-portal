5369853b38387f3d63118a405f80409c63de49f9
reyemtm <malcolmtmeyer@gmail.com>
2017-06-09 12:06:38 -0400

    update

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,10 +1,11 @@
 ---
 ---
 {% include map-plugins/mapzen.min.js%}
-{% include map-plugins/easy-button.js%}
 {% include map-plugins/leaflet-omnivore.min.js%}
 {% include map-plugins/Control.Fullscreen.js%}
 {% include map-plugins/hash.js%}
 {% include map-plugins/Leaflet.VectorGrid.bundled.js %}
 {% include map-plugins/leaflet-sleep.js %}
-{% include map-plugins/leaflet-sidebar.min.js%}
+{% include map-plugins/node_modules/leaflet-sidebar/src/L.Control.Sidebar.js%}
+{% include map-plugins/node_modules/leaflet-easybutton/src/easy-button.js.%}
+{% include map-plugins/node_modules/leaflet.locatecontrol/dist/L.Control.Locate.min.js%}
