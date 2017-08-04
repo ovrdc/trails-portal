034b2df0e1fa9ffcfd7cf0c8c1902efc10ff3a34
reyemtm <malcolmtmeyer@gmail.com>
2017-08-04 11:59:59 -0400

    t

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,6 +1,8 @@
 ---
 ---
+{% if page.permalink == "mapzen" %}{% else %}
 {% include map-plugins/node_modules/leaflet/dist/leaflet.js%}
+{% endif %}
 /*adding omnivore directly since its node module dependencies are insance*/
 {% include map-plugins/leaflet-omnivore.min.js%}
 {% include map-plugins/node_modules/leaflet-hash/leaflet-hash.js%}
